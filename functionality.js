de812c8bb028837201a3ed1deca0c3aca88ba201
SarojKumar10 <sarojkr699681@gmail.com>
2020-01-25 07:25:13 +0530

    Update functionality.js

---
+++ functionality.js
@@ -0,0 +1,7 @@
+function homeOnClick(){
+	window.location.assign("http://www.sarojgames.technology");
+}
+
+function githubOnClick(){
+	window.location.assign("http://www.github.com/SarojKumar10);
+}
