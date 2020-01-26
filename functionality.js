8ac99807b6fb3209230776d3d1222d141bfa2827
SarojKumar10 <sarojkr699681@gmail.com>
2020-01-26 15:05:51 +0530

    Updated ractor.html

--- functionality.js
+++ functionality.js
@@ -2,6 +2,22 @@ function homeOnClick(){
 	window.location.assign("http://www.sarojgames.technology")
 }
 
+function gamesOnClick() {
+	window.location.assign("http://www.sarojgames.technology/pages/games.html")
+}
+
+function learnOnClick() {
+	window.location.assign("http://www.sarojgames.technology/pages/learn.html")
+}
+
+function toolsOnClick() {
+	window.location.assign("http://www.sarojgames.technology/pages/tools.html")
+}
+
+function aboutOnClick() {
+	window.location.assign("http://www.sarojgames.technology/pages/about.html")
+}
+
 function ractorOnClick() {
 	window.location.assign("http://www.sarojgames.technology/pages/ractor.html")
 }
